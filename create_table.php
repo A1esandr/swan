49cbb1211a579d5035e9886079d9430a654fe95b
Alexander Kurakin <A1esandr@users.noreply.github.com>
2015-11-07 01:45:22 +0500

    Create create_table.php

---
+++ create_table.php
@@ -0,0 +1,18 @@
+<?php
+require_once ('MysqliDb.php');
+require_once ('config.php');
+
+$db = new MysqliDb (DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+$users = $db->rawQuery(
+"CREATE TABLE
+    `users` (
+        `login` CHAR(50) NOT NULL,
+        `password` CHAR(150) NOT NULL,
+        `name` CHAR(30) NOT NULL,
+        `email` CHAR(50) NOT NULL,
+        PRIMARY KEY(`login`)
+    )");
+
+echo 'ok';
+?>
